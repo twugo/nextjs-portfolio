f7ed6439b2c47a29f91413cc920c05985099fa18
twugo <twugo@users.noreply.github.com>
2022-04-26 04:07:04 +0000

    deploy: 4751c3df179602e6ab73ea747e9f299c764ab502

---
+++ _next/static/-TxJlUUcs_LowQ79vY0Ra/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/458-6e6e18a0405b4265.js","static/chunks/pages/index-4bd842dd54a0b1e7.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
