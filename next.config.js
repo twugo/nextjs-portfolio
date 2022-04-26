27e611b61d0feb804a2889d32d1c91b1cd284b54
Twugo <hm17031@student.miyazaki-u.ac.jp>
2022-04-26 13:04:16 +0900

    Update: setting for export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // for 'next export'
+  images: {
+    loader: 'akamai',
+    path: '/'
+  }
 }
 
 module.exports = nextConfig
